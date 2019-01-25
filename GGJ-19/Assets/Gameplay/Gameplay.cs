e392beb5ff65d848d4df8c2028987c8e0a3287a3
MadrMan <madman_49@hotmail.com>
2019-01-26 00:16:08 +0100

    Bare core loop states

---
+++ GGJ-19/Assets/Gameplay/Gameplay.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Gameplay
+{
+    public class Gameplay : MonoBehaviour
+    {
+        ILoopState state;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            ChangeState(new BreakState());
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void ChangeState(ILoopState nextState)
+        {
+            if (state != null)
+            {
+                state.Exit();
+            }
+            
+            
+            state = nextState;
+
+            if (state != null)
+            {
+                state.Enter();
+            }
+        }
+    }
+}
---
+++ GGJ-19/Assets/Gameplay/WinState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Gameplay
+{
+    public class WinState : ILoopState
+    {
+        public void Enter()
+        {
+        }
+
+        public void Exit()
+        {
+        }
+    }
+}
