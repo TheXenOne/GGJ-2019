30368ce0962f0ce3757bf132a24f676d19375ce7
Erik <35656626+SeleckyErik@users.noreply.github.com>
2019-01-26 16:16:39 +0100

    [ADD] Player.cs - script containing player gameplay functions; PlayerCharacter prefab - attached Player.cs

---
+++ GGJ-19/Assets/Components/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Gameplay;
+using Assets.Components;
+
+public class Player : MonoBehaviour
+{
+    public Gameplay gameplay;
+    public static Player Instance;
+    
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Respawn(GameObject wagonComponent)
+    {
+        Bounds bounds = wagonComponent.GetComponent<Collider>().bounds;
+
+        transform.position = bounds.center + Vector3.up * bounds.extents.y;
+    }
+
+    public void RespawnRandom()
+    {
+        var wagon = gameplay.caravan.GetComponent<Caravan>().GetRandomWagon();
+
+        Respawn(wagon.GetComponent<CaravanWagon>().GetRandomActiveComponent());
+    }
+}
