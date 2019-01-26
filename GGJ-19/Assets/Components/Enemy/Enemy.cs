60a05729052c8c2c6a6f12ed56f0c0ba72062d44
MadrMan <madman_49@hotmail.com>
2019-01-26 22:15:59 +0100

    Forward enemytype to enemy

--- GGJ-19/Assets/Components/Enemy/Enemy.cs
+++ GGJ-19/Assets/Components/Enemy/Enemy.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Assets.Gameplay.Spawning;
 using UnityEngine;
 
 public class Enemy : Character
 {
+    public EnemyType m_enemyType;
+
+    private void Start()
+    {
+    }
+
     public override void EventDied()
     {
         Debug.Log("Enemy Died!");
