720c0e102a8a89bdcd4260ccf4bce70b4ead3cfb
Ben Landor <benlandor@live.com>
2019-01-26 11:15:12 +0100

    - Added State and StateMachine generics. - Added EnemyAI component. - Added StateClimb for EnemyAI component.

---
+++ GGJ-19/Assets/Gameplay/Enemies/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public StateMachine<EnemyAI> enemyStateMachine;
+
+    void Awake()
+    {
+        enemyStateMachine = new StateMachine<EnemyAI>(this, StateClimb.Instance);
+    }
+
+    void Update()
+    {
+        enemyStateMachine.UpdateStateMachine();
+    }
+}
---
+++ GGJ-19/Assets/Gameplay/Enemies/StateClimb.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateClimb : State<EnemyAI>
+{
+    private static StateClimb instance;
+
+    private StateClimb() { }
+
+    public static StateClimb Instance {
+        get {
+            if (instance == null)
+            {
+                instance = new StateClimb();
+            }
+            return instance;
+        }
+    }
+
+    public override void Enter(EnemyAI a_entity)
+    {
+        Debug.Log("Entered StateClimb.");
+    }
+
+    public override void Execute(EnemyAI a_entity)
+    {
+        
+    }
+
+    public override void Exit(EnemyAI a_entity)
+    {
+    }
+}
