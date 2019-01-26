b32ba6b64512697f81fbdb5986f87f36477f042d
Erik <35656626+SeleckyErik@users.noreply.github.com>
2019-01-26 02:08:22 +0100

    [ADD] PlayerCharacter prefab and PlayerMovement component script

---
+++ GGJ-19/Assets/Components/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 100.0f;
+    public float jumpStrength = 100.0f;
+
+    void ProcessInput()
+    {
+    }
+
+    void ResolveInput()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        ResolveInput();
+    }
+}
