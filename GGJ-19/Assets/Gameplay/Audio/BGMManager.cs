6d751895b4094a469f22998d1165658dba35b739
Ben Landor <benlandor@live.com>
2019-01-27 02:20:35 +0100

    - Added audio BGM and BGM manager.

---
+++ GGJ-19/Assets/Gameplay/Audio/BGMManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public AudioSource sourceFight;
+    public AudioSource sourceCalm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AudioController.FadeIn(sourceFight, 1f));
+    }
+
+    public void PlayFightMusic()
+    {
+        StartCoroutine(AudioController.FadeOut(sourceCalm, 1f));
+        StartCoroutine(AudioController.FadeIn(sourceFight, 1f));
+    }
+}
